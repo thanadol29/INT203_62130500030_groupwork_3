4fcf7ecb51be0f81201fa10bab3a4dd97266342c
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-02-13 18:25:10 +0700

    Add
    add functional website

--- 62130500030_groupwork_3/test2.js
+++ 62130500030_groupwork_3/test2.js
@@ -1,25 +1,25 @@
 const app = {
   data() {
       return {
+          
           place1:'Moscow',
           url1:'https://i.imgur.com/B0zvdp6.png',
           place2:'Berlin',
           url2:'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Berlin_reichstag_west_panorama_2.jpg/1920px-Berlin_reichstag_west_panorama_2.jpg',
-          place3:"",
-          url3:'',
-          heart:function (event) {
-            
-            alert('<3')}
-        
+          place3:"Vienna",
+          url3:'https://i.imgur.com/5tRu76I.jpg',
+          favorite1: false,
+          favorite2: false,
+          favorite3: false,
+          count: "3",
+          test: {
+            place:'mascow',
+            url:'https://i.imgur.com/B0zvdp6.png'
+          }
       }
       
   },
-  created(){
-      console.log('test '+ this.msg)
-  },
-  updated(){
-      console.log('Message is changed  to '+ this.msg)
-  }
+  
 }
 
 
